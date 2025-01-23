25acbb156bfc6d2c7fdad6afe47862b72d9ade69
AdiAnteater <150882784+AdiAnteater@users.noreply.github.com>
2025-01-22 23:15:21 -0800

    hmmmm

--- src/unordered_list.h
+++ src/unordered_list.h
@@ -6,16 +6,16 @@ struct UnorderedList
 {
     const string name;
     UnorderedList(const string & new_name)
-        : name(new_name) { }
+        : name(new_name){}
     virtual void insert(const string & word) = 0;
-    virtual void find(const string & word) = 0;
+    virtual bool find(const string & word) = 0;
     virtual void remove(const string & word) = 0;
     virtual bool is_empty() = 0;
     virtual bool is_full() = 0;
     virtual void print(ostream & out) = 0;
     UnorderedList(const UnorderedList & L) = delete;
     UnorderedList& operator =(const UnorderedList & L) = delete;
-    virtual ~UnorderedList() { }
+    virtual ~UnorderedList() {}
 };
 
 class UnorderedArrayList
@@ -28,8 +28,8 @@ class UnorderedArrayList
 public:
     UnorderedArrayList(int cap = NWORDS);
     void insert(const string & word);
-    void find(const string & word);
-    void remove(const string & words);
+    bool find(const string & word);
+    void remove(const string & word);
     bool is_empty();
     bool is_full();
     void print(ostream & out);
@@ -41,12 +41,13 @@ struct ListNode
     string data;
     ListNode * next;
     ListNode(string new_data, ListNode * new_next)
-        : data(new_data), next(new_next) { }
+        :data(new_data),next(new_next) { }
     static void print(ostream & out, ListNode * L);
-    static ListNode * find(const string &  word, ListNode * L);
+    static ListNode * find(const string & word, ListNode * L);
     static void delete_list(ListNode * L);
-    static void remove(const string & word, ListNode * & L);
+    static void remove(const string & word,ListNode * & L);
 };
+
 class UnorderedLinkedList
     : public UnorderedList
 {
@@ -55,12 +56,13 @@ public:
     UnorderedLinkedList();
     void insert(const string & word);
     bool find(const string & word);
-    void remove(const string &  word);
+    void remove(const string & word);
     bool is_empty();
     bool is_full();
     void print(ostream & out);
     ~UnorderedLinkedList();
 };
+
 ostream & operator << (ostream & out, UnorderedList & L);
 
 void error(string word, string msg);
